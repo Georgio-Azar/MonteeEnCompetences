72f1d7efbe0d2bdaac79768a5e86a586498faf88
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-05 16:50:04 +0200

    feat!: Addition of the possibility to get the information of a single user

--- server.js
+++ server.js
@@ -28,4 +28,24 @@ app.get('/users', (req, res) => {
     }
 });
 
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    try {
+        console.log('Reading file...');
+        const data = fs.readFileSync('utilisateurs.json', 'utf8');
+        console.log('File read successfully');
+        const users = JSON.parse(data);
+        const user = users.find(user => user.id == id);
+        if (user) {
+            res.send(`Nom: ${user.nom}, Prenom: ${user.prenom}, Age: ${user.age}, Mail : ${user.email}`);
+        } else {
+            res.status(404).send('User not found');
+        }
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Error reading file');
+    }
+});
+
 app.listen(PORT, () => {});

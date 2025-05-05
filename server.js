0f4374e21f2794f0207fce4dead86231b81dc39b
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-05 17:25:35 +0200

    feat! : Creation of a new route that allows us to create users

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import fs from 'fs';
+import crypto from 'crypto';
 
 const app = express();
 const PORT = 3000;
@@ -45,6 +46,29 @@ app.get('/users/:id', (req, res) => {
     catch (err) {
         console.error(err);
         res.status(500).send('Error reading file');
+    }
+});
+
+
+app.post('/users', express.json(), (req, res) => {
+    const newUser = req.body;
+    try {
+        console.log('Reading file...');
+        if (newUser.nom === undefined || newUser.prenom === undefined || newUser.age === undefined || newUser.email === undefined) {
+            res.status(400).send('Missing user information');
+            return;
+        }
+        const data = fs.readFileSync('utilisateurs.json', 'utf8');
+        console.log('File read successfully');
+        const users = JSON.parse(data);
+        newUser.id = crypto.randomUUID();
+        users.push(newUser);
+        fs.writeFileSync('utilisateurs.json', JSON.stringify(users, null, 2));
+        res.status(201).send('User added successfully');
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Error writing file');
     }
 });
 

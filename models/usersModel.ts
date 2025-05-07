c75f34b9dd47d4c6539efe835949872a244b993c
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-07 09:34:10 +0200

    feat!: Creation of a new function checkPassword that will use regular expressions in order to test the validity of a password, and return the error message

--- models/usersModel.ts
+++ models/usersModel.ts
@@ -6,6 +6,33 @@ async function hashPassword(password : string): Promise<string> {
     return hash;
 }
 
+function checkPassword(password : string) : string {
+    if (password.length < 12) {
+        return 'Password must be at least 12 characters long';
+    }
+    const checks = [
+        /[a-z]/,
+        /[A-Z]/,
+        /[0-9]/,
+        /[@.#$!%*?&]/
+    ];
+
+    const checkNames = [
+        'lowercase letter',
+        'uppercase letter',
+        'number',
+        'special character'
+    ];
+
+    for (let i = 0; i < checks.length; i++) {
+        if (!checks[i].test(password)) {
+            return `Password must contain at least one ${checkNames[i]}`;
+        }
+    }
+    return "";
+}
+
 export default {
-    hashPassword
+    hashPassword,
+    checkPassword
 };

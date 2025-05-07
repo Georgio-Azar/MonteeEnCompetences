6abe6f4e9e21acad7ea50504a7218270ded74a3f
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-07 09:13:15 +0200

    fix: Creation of the Zod schema to modify a user

--- schemas/usersSchemas.ts
+++ schemas/usersSchemas.ts
@@ -8,3 +8,12 @@ export const addUserSchema = z.object({
     email : z.string().email(),
     password : z.string().min(8)
 })
+
+export const modifyUserSchema = z.object({
+    id : z.string().uuid().optional(),
+    nom : z.string().min(1).optional(),
+    prenom : z.string().min(1).optional(),
+    age : z.number().int().positive().optional(),
+    email : z.string().email().optional(),
+    password : z.string().min(8).optional()
+})

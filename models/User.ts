3027d2eee05895b7e16d76a32129eac8cf6caced
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-09 10:12:31 +0200

    feat: Creation of the table user in the database

---
+++ models/User.ts
@@ -0,0 +1,39 @@
+import { Sequelize, DataTypes } from "sequelize";
+import User from "../types/User";
+
+export default (sequelize: Sequelize) => {
+    const user = sequelize.define<any, User>(
+        "user",
+        {
+            id : {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                allowNull: false,
+                defaultValue: DataTypes.UUIDV4,
+            },
+            nom: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            prenom: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            age: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        { timestamps: true }
+    );
+    return user;
+}

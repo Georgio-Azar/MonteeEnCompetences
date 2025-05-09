c49a8610cfef59b2063f9ad66f77f465e5a25525
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-09 10:14:11 +0200

    feat: Connection to the database

--- server.ts
+++ server.ts
@@ -3,8 +3,21 @@ import express from 'express';
 import index from './routes/index';
 import users from './routes/users';
 
+import db from './database';
+
 const app = express();
 const PORT = 3000;
+
+(async () => {
+    try {
+        await db.sequelize.authenticate();
+        db.sequelize.sync({ force: true });
+        console.log('Connection has been established successfully.');
+    }
+    catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
 
 app.use("/", index);
 app.use("/users", users);

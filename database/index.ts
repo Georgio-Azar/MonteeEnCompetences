f278eed53976a749da9ad7dc8cf0074aa64479f1
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-15 09:20:14 +0200

    fix: Usage of the .env file to store the sensible information of the database

--- database/index.ts
+++ database/index.ts
@@ -8,11 +8,11 @@ console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 export const sequelize = new Sequelize({
     dialect: 'postgres',
-    host: 'database',
-    username: 'root',
-    password: 'root',
-    database: 'database',
-    port: 5432,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
     pool: {
         max: 5,
         min: 0,

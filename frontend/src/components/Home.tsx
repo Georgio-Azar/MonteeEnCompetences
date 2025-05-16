b1f919c71eebbdc9f98f95fe33c2c0405f6058b0
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-16 10:08:25 +0200

    fix: Separation of the frontend and the backend

--- frontend/src/components/Home.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div id="home">
-            <h1>Welcome to the Home Page</h1>
-            <p>What do you want to do ?</p><br />
-        </div>
-    )
-}
--- frontend/src/components/Profile.tsx
+++
@@ -1,7 +0,0 @@
-export default function Profile() {
-    return (
-    <div id="profile">
-        <p>Profile info</p>
-    </div>
-    );    
-}
